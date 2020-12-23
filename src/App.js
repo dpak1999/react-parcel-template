59c23543bc9721532f3d20f26bfc08a2fa2179d9
dpak1999 <deepakkumardash313@gmail.com>
2020-12-23 12:36:38 +0530

    Add : configuration

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>
+        Hello from parcel!!
+        <br />
+        👋👋
+      </h1>
+    </div>
+  );
+};
+
+export default App;
